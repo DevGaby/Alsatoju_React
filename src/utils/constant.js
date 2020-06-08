4c508a9d14f39a3eb877aea0c0b23ba6219dda2c
Alan <alan@hypecode.com>
2020-06-08 13:58:10 +0200

    update constant

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,8 @@
 export const API_URL = process.env.API_URL || 'https://alsatoju-dev.herokuapp.com'
 
+export const TOKEN = 'token'
+export const USER = 'user'
+
 export const LOGIN = `${API_URL}/login`
+export const USERS = `${API_URL}/app_users`
+export const MY_PROFIL = `${USERS}/myProfile`
